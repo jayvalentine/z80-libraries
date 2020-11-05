5beb2237d89979510ff049dda63580825bf7a933
jayvalentine <jayv136@gmail.com>
2020-11-05 10:59:15 +0000

    Removed FILE definition from stdio

--- include/stdio.h
+++ include/stdio.h
@@ -2,6 +2,24 @@
 // Implementation for Modular-Z80 platform.
 // Copyright (c) Jay Valentine 2020.
 
+#ifndef _STDIO_H
+#define _STDIO_H
+
+#include <stdint.h>
+#include <stddef.h>
+
+typedef struct _IO_FILE
+{
+    uint32_t size;
+    uint32_t fpos; /* Not actually used for indexing the file, just for keeping track of size. */
+    
+    uint16_t start_cluster;
+    uint16_t current_cluster;
+
+    uint8_t sector; /* Limitation - can't handle more than 256 sectors per cluster. */
+    uint16_t fpos_within_sector;
+} FILE;
+
 #define NULL 0
 #define EOF -1
 
@@ -26,3 +44,22 @@ char *   gets(char *s) __z88dk_fastcall;
 // Currently only the most basic %u (unsigned integer) format is provided,
 // with no provision for spacing/padding.
 int     printf(const char *format, ...);
+
+/* fopen
+ * Opens a file in the given mode.
+ * Returns a handle to that file.
+ */
+FILE * fopen(const char * filename, const char * mode);
+
+/* fclose
+ * Closes an open file.
+ */
+int fclose(FILE * stream);
+
+/* fread
+ * Reads a number of generic data items from a stream.
+ * Returns the number of items read.
+ */
+size_t fread(void * ptr, size_t size, size_t n, FILE * stream);
+
+#endif /* STDIO_H */
