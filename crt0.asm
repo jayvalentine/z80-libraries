dae181a599b9015303a530fae00fb1d8f99bcaf8
Jay Valentine <jayv136@gmail.com>
2021-11-06 15:30:15 +0000

    Tweaked crt0

--- crt0.asm
+++ crt0.asm
@@ -1,7 +1,14 @@
     ; CRT0 for modularz80 platform.
     ; For running code in user-memory.
-    EXTERN  _main
+    ;
+    ; NOTE: The label _user_main is used because
+    ; z88dk forces the main() function to use
+    ; the stdc calling convention.
+    ;
+    ; Naming the function something different
+    ; allows us to use the normal calling
+    ; convention.
+    EXTERN  _user_main
 
 start:
-    call    _main
-    ret
+    jp    _user_main
