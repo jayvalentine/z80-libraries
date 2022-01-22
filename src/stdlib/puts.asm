666170eb9f86388d5b24f7fc6cdeece03cb56885
Jay Valentine <jayv136@gmail.com>
2022-01-22 12:37:10 +0000

    Updated puts() to use the new swrite form

--- src/stdlib/puts.asm
+++ src/stdlib/puts.asm
@@ -9,25 +9,25 @@ define(zsys, `ld      A, $1 << 1
     defc    SWRITE = 0
     defc    SREAD  = 1
 
+    EXTERN  _strlen
+
 _puts:
     push    HL
-    pop     DE
 
-__puts_loop:
-    ; src in DE.
-    ld      A, (DE)
-    inc     DE
-    cp      0
-    jp      z, __puts_done
+    ; Get string length (in HL).
+    call    _strlen
 
-    ; Not null, print to serial port.
-    ld      L, A
-    call    _putchar
+    ; Move length into BC.
+    push    HL
+    pop     BC
 
-    ; Loop.
-    jp      __puts_loop
+    pop     DE ; String pointer.
 
-__puts_done:
+    ; Write to serial port.
+    ; DE - string pointer
+    ; BC - length
+    zsys(SWRITE)
+
     ; Successful, return 0.
     ld      HL, 0
     ret
