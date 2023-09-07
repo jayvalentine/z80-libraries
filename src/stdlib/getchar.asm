a846a58077fce4130cfdca3dc6af743e3788b864
Jay Valentine <jayv136@gmail.com>
2023-09-07 22:45:56 +0100

    fixing putchar and getchar

--- src/stdlib/getchar.asm
+++ src/stdlib/getchar.asm
@@ -1,12 +1,5 @@
     .globl  _getchar
 
 _getchar:
-    push    AF
-    push    IX
     ld      A, #2
-    rst     48
-    ld      D, #0
-    ld      E, A
-    pop     IX
-    pop     AF
-    ret
+    jp      48
--- src/stdlib/putchar.asm
+++ src/stdlib/putchar.asm
@@ -6,13 +6,17 @@
     ;
     ; NOT REENTRANT!
 _putchar:
-    ld      (__putchar_tempchar), HL
+    ld      A, L
+    ld      (__putchar_tempchar), A
     ld      HL, #__putchar_tempchar
     ld      DE, #1
 
     ld      A, #0
     rst     48
     
+    ld      A, (__putchar_tempchar)
+    ld      L, A
+    ld      H, #0
     ret
 
 __putchar_tempchar:
