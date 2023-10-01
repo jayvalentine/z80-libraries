4d168311b07ff99ea2ec53dbfef76514c9e71fa9
Jay Valentine <jayv136@gmail.com>
2023-10-01 09:15:24 +0100

    crt0 now sets default SIG_CANCEL handler

--- process_crt0.asm
+++ process_crt0.asm
@@ -13,12 +13,27 @@
     .globl  _syscall_pexit
 
 start:
+    ; Register default SIG_CANCEL handler.
+    ld      HL, #_default_cancel
+    ld      DE, #0
+    call    _syscall_sighandle
+
+    ; Set up arguments for main().
+
     ; argc at 0xf800.
     ld      DE, (0xf800)
     
     ; argv at 0xf810
     ld      HL, #0xf810
 
+    ; Call main() and exit process with returned
+    ; value.
     call    _user_main
     ex      DE, HL
+    call    _syscall_pexit
+
+    ; Default SIG_CANCEL handler.
+    ; Exits with exit code of -1.
+_default_cancel:
+    ld      HL, #-1
     call    _syscall_pexit
