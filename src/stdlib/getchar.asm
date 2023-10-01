6c162e024847d564bbe5d917e396078f0ee44251
Jay Valentine <jayv136@gmail.com>
2023-10-01 10:09:42 +0100

    getchar() now waits for available character

--- src/stdlib/getchar.asm
+++ src/stdlib/getchar.asm
@@ -1,5 +1,14 @@
     .globl  _getchar
 
+    ; int getchar(void);
 _getchar:
     ld      A, #2
-    jp      48
+    rst     48
+
+    ; sread syscall returns -1 if no character available.
+    ld      A, D
+    cp      #0xff
+    jp      z, _getchar
+
+    ; otherwise returned character is in DE.
+    ret
